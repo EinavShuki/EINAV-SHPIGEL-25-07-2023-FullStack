3d6ca1555b2264ae754438a40a011bfdfca5ac6c
einav <einavdev@gmail.com>
2023-07-25 18:19:21 +0300

    adding loader

--- frontend/src/components/WeatherCard/WeatherCard.jsx
+++ frontend/src/components/WeatherCard/WeatherCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './WeatherCard.css';
 import _ from 'lodash';
 import useFetch from '../../hooks/useFetch';
+import Loader from '../Loader/Loader';
 
 function WeatherCard({ currentLocation }) {
   console.log({ currentLocation });
@@ -16,28 +17,32 @@ function WeatherCard({ currentLocation }) {
 
   return (
     <div className='weather_area'>
-      {data && (
-        <div>
-          <div className='weather_top_line'>
-            <h3
-              style={{
-                borderBottom: 'solid 1px  hsl(0, 0%, 40%)',
-                paddingBottom: '5px',
-              }}>
-              Current Weather
-            </h3>
-            <h5 className='date'>{formattedDate}</h5>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        data && (
+          <div>
+            <div className='weather_top_line'>
+              <h3
+                style={{
+                  borderBottom: 'solid 1px  hsl(0, 0%, 40%)',
+                  paddingBottom: '5px',
+                }}>
+                Current Weather
+              </h3>
+              <h5 className='date'>{formattedDate}</h5>
+            </div>
+            <div className='weather_mid_line'>
+              <img
+                style={{ width: '15vw' }}
+                src={require(`../../images/${data.WeatherIcon}.svg`)}
+                alt={data.WeatherText}
+              />
+              <div className='conditon'>{data.WeatherText}</div>
+            </div>
+            <div className='temp'>{currentTemp}&deg;</div>
           </div>
-          <div className='weather_mid_line'>
-            <img
-              style={{ width: '15vw' }}
-              src={require(`../../images/${data.WeatherIcon}.svg`)}
-              alt={data.WeatherText}
-            />
-            <div className='conditon'>{data.WeatherText}</div>
-          </div>
-          <div className='temp'>{currentTemp}&deg;</div>
-        </div>
+        )
       )}
     </div>
   );
